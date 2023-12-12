79f6c22a8f2123f2bc5235102c0583bc862e38b1
Biel Sala <bielsalamimo@gmail.com>
2023-12-12 12:58:42 +0100

    Emacs dotfiles

---
+++ emacs/emacs.d/elpa/diredful-20160529.2017/diredful-pkg.el
@@ -0,0 +1,12 @@
+(define-package "diredful" "20160529.2017" "colorful file names in dired buffers" 'nil :commit "b17b3087e0084a5571a9ac4d47ccfc36d96b109e" :authors
+  '(("Thamer Mahmoud" . "thamer.mahmoud@gmail.com"))
+  :maintainers
+  '(("Thamer Mahmoud" . "thamer.mahmoud@gmail.com"))
+  :maintainer
+  '("Thamer Mahmoud" . "thamer.mahmoud@gmail.com")
+  :keywords
+  '("dired" "colors" "extension" "widget")
+  :url "https://github.com/thamer/diredful")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
